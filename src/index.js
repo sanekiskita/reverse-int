d3f7aeb9a7de94e9d02089aadcaa593322f70e0a
sanekiskita <as1405503@gmail.com>
2020-10-08 17:08:43 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n = n.toString();
+    var str = '';
+    for (var i = 0; i <= n.length; i++) {
+        str = str + n.substr(n.length - i, 1);
+    }
+    return parseInt(str);
 }
